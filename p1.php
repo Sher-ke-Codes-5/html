fd0b2920adaf12416a416ad557de528f7881fd30
Sher-ke-Codes-5 <85151135+Sher-ke-Codes-5@users.noreply.github.com>
2021-10-02 01:20:16 +0500

    Add files via upload

--- p1.php
+++ p1.php
@@ -1,25 +1,23 @@
+<!DOCTYPE html>
+<html>
 
 
 
+<body>
 
 
+<form action="signin.php" method="POST" style="background-color: silver; color:lightblue">
+    <h2 > Signing in <h2>
+        Name : 
+    <input type="text" name="Name"  placeholder="enter name" ><br>
+    Email : 
+    <input type="text" name="Email"  placeholder ="enter email" ><br>
+<br>
+    <input type="submit"  value ="subm" style="font-size:20px ; padding-right:20px "><br>
+<form>
 
+    
 
-
-
-
-       <?php
-            session_start();
-            echo" session start";
-            $_SESSION["X"]="Sherjan";
-            $_SESSION["Y"]="Inam";
-            setcookie("country","Pak",time()+(86400*30));
-
-
-
-            ?>
-
-
-
-
-
+</body>
+    
+</html>
